f6a8136bc326917c0df225fd8bdd1240271e827c
Maxim Faleev <m.faleev@innopolis.university>
2025-04-29 13:47:40 +0200

    liner

--- backend/app/core/session.py
+++ backend/app/core/session.py
@@ -1,10 +1,11 @@
 from contextlib import contextmanager
 from app.core.database import SessionLocal
 
+
 @contextmanager
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close() 
+        db.close()
