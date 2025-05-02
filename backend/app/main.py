5670db4b8be50b24af4f71806c8c2af93de49842
Maxim Faleev <65727242+implausibleDeniability@users.noreply.github.com>
2025-05-02 10:53:42 +0200

    backend: add auth endpoint and User ORM (#5)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.database import engine
+from app.models.user import User
+from app.auth_view import router as auth_router
 
+User.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url="/openapi.json",
 )
+
+app.include_router(auth_router, prefix="/api", tags=["auth"])
 
 
 @app.get("/health")
