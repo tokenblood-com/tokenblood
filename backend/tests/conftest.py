241f34878f9eb5222616a9b9264190b1e841f1b5
Shamil Arslanov <sh.arslanov.work@gmail.com>
2025-05-18 14:11:33 +0300

    move mock data into code

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,3 @@
-import json
 import os
 from app.models.submission import Submission
 from app.models.user import User
@@ -6,40 +5,26 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
-from datetime import datetime
 from app.core.database import Base
-from pyrootutils import find_root
 from app.main import app
 from fastapi.testclient import TestClient
 from app.core.session import get_db
+from app.mock_data.users import user1, user2, user3
+from app.mock_data.submissions import submission1, submission2, submission3, submission4
 
 
 @pytest.fixture(scope="session")
-def test_users() -> list[User]:
-    data_path = find_root() / "tests" / "test_data" / "users.json"
-    with open(data_path, "r") as f:
-        users = json.load(f)
-
-    users = [User(**user) for user in users]
-    return users
+def mock_users() -> list[User]:
+    return [user1(), user2(), user3()]
 
 
 @pytest.fixture(scope="session")
-def test_submissions() -> list[Submission]:
-    data_path = find_root() / "tests" / "test_data" / "submissions.json"
-    with open(data_path, "r") as f:
-        submissions = json.load(f)
-
-    for submission in submissions:
-        submission["created_at"] = datetime.strptime(submission["created_at"], "%Y-%m-%d %H:%M:%S")
-        submission["updated_at"] = datetime.strptime(submission["updated_at"], "%Y-%m-%d %H:%M:%S")
-
-    submissions = [Submission(**submission) for submission in submissions]
-    return submissions
+def mock_submissions() -> list[Submission]:
+    return [submission1(), submission2(), submission3(), submission4()]
 
 
 @pytest.fixture(scope="session")
-def test_db(test_users, test_submissions):
+def test_db(mock_users, mock_submissions):
     test_db_url = "sqlite:///./test.db"
     engine = create_engine(test_db_url, connect_args={"check_same_thread": False})
     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -55,11 +40,11 @@ def test_db(test_users, test_submissions):
 
     app.dependency_overrides[get_db] = override_get_db
 
-    # Insert test users and submissions into the database
+    # Insert mock users and submissions into the database
     db: Session = TestingSessionLocal()
     try:
-        db.add_all(test_users)
-        db.add_all(test_submissions)
+        db.add_all(mock_users)
+        db.add_all(mock_submissions)
         db.commit()
     finally:
         db.close()
